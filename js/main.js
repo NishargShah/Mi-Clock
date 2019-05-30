ab11f0b12eca32334096daf03205b3516f4bedd9
NishargShah <nishargshah3101@gmail.com>
2019-05-31 02:42:03 +0530

    convert all id to class

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 var clock = "";
 
 for (var i = 0; i < 360; i++) {
-    clock += "<span id='dials'><div id='cutline'></div></span>";
+    clock += "<span class='dials'><div class='cutline'></div></span>";
 }
-clock += "<div id='dot'></div>";
-clock += "<div id='mline'></div>";
-clock += "<div id='hline'></div>";
-clock += "<div id='tringleM'><div id='tringle'></div><div class='shadow'></div></div>";
+clock += "<div class='dot'></div>";
+clock += "<div class='mline'></div>";
+clock += "<div class='hline'></div>";
+clock += "<div class='tringleM'><div class='tringle'></div><div class='shadow'></div></div>";
 
-document.getElementById('container').innerHTML = clock;
+document.querySelector('.container').innerHTML = clock;
 
 for (var i = 1; i <= 360; i++) {
-    document.querySelector('#dials:nth-child(' + i + ')').style.transform = "rotate(" + (i - 1) + "deg)";
+    document.querySelector('.dials:nth-child(' + i + ')').style.transform = "rotate(" + (i - 1) + "deg)";
 }
 
 /* Real Time Clock */
@@ -23,13 +23,13 @@ setInterval(function () {
     hour = moment().hours();
     min = moment().minutes() * 6;
     hour = moment().hours() % 12 / 12 * 360 + (min / 12);
-    lol = document.getElementById('mline').style.transform = "rotate(" + (180 + min) + "deg)";
-    lol1 = document.getElementById('hline').style.transform = "rotate(" + (180 + hour) + "deg)";
+    lol = document.querySelector('.mline').style.transform = "rotate(" + (180 + min) + "deg)";
+    lol1 = document.querySelector('.hline').style.transform = "rotate(" + (180 + hour) + "deg)";
 }, 1000);
 
 setInterval(function () {
     second++;
-    lol2 = document.getElementById('tringleM').style.transform = "rotate(" + (180 + second) + "deg)";
-    document.querySelector('#container span:nth-of-type(' + ((second - 4) % 360 + 1) + ') #cutline').classList.add('one');
-    document.querySelector('#container span:nth-of-type(' + ((second - 30 + 360) % 360 + 1) + ') #cutline').classList.remove('one');
+    lol2 = document.querySelector('.tringleM').style.transform = "rotate(" + (180 + second) + "deg)";
+    document.querySelector('.container span:nth-of-type(' + ((second - 4) % 360 + 1) + ') .cutline').classList.add('one');
+    document.querySelector('.container span:nth-of-type(' + ((second - 30 + 360) % 360 + 1) + ') .cutline').classList.remove('one');
 }, (60 / 360) * 1000);
